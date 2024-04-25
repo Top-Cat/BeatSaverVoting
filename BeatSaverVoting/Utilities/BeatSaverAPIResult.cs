0e90af33f5797465b49bb33956243e6918507767
qe201020335 <qe201020335@sina.com>
2024-04-25 15:57:02 -0400

    remove unused Song fields

--- BeatSaverVoting/Utilities/BeatSaverAPIResult.cs
+++ BeatSaverVoting/Utilities/BeatSaverAPIResult.cs
@@ -6,16 +6,10 @@ namespace BeatSaverVoting.Utilities
     [Serializable]
     public class Song
     {
-        public string levelAuthorName;
-        public string songAuthorName;
-        public string songName;
-        public string songSubName;
         public int upVotes;
         public int downVotes;
         public string key;
         public string hash;
-
-        public string path;
 
         public Song()
         {
@@ -36,16 +30,6 @@ namespace BeatSaverVoting.Utilities
         public bool Compare(Song compareTo)
         {
             return compareTo.hash == hash;
-        }
-
-        public Song(BeatmapLevel data)
-        {
-            songName = data.songName;
-            songSubName = data.songSubName;
-            songAuthorName = data.songAuthorName;
-            levelAuthorName = "";// data.levelAuthorName; // TODO: BeatmapLevel.allMappers and allLighters combined?
-            path = "";// data.customLevelPath; // This is not used.
-            hash = SongCore.Collections.hashForLevelID(data.levelID).ToLower();
         }
     }
 }
